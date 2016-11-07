6e7e257b1b3c04eebf0dcc6ae5c5195c844486cd
Lifeng Li <lfli@marvell.com>
2016-11-07 11:07:04 -0800

    add filelist.tcl

---
+++ filelist.tcl
@@ -0,0 +1,7 @@
+set package_dir [file dirname [info script]] ;
+source $package_dir/qf_detect_tool
+source $package_dir/qf_define_proc_arguments
+source $package_dir/qf_parse_proc_arguments
+source $package_dir/qf_get_proc_name
+source $package_dir/qf_create_menu
+
